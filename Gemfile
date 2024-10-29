f9620a2a78921521a39a2b6521e5dd5338813f57
tchan1002 <tchan0@uchicago.edu>
2024-10-29 00:37:15 +0000

    init commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# /Gemfile
+source "https://rubygems.org"
+
+gem "puma"
+gem "rackup"
+gem "sinatra"
+gem "sinatra-contrib"
+gem "better_errors"
+gem "binding_of_caller"
---
+++ dice.rb
@@ -0,0 +1,79 @@
+# /dice.rb
+
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+get("/") do
+  erb(:elephant)
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>
+   <a href='/'> <p> Back to Home </p> </a>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>
+  <a href='/'> <p> Back to Home </p> </a>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  outcome = "You rolled a #{first_die}."
+  
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>
+  <a href='/'> <p> Back to Home </p> </a>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die
+
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, and #{fourth_die} for a total of #{sum}"
+  "<h1>5d4</h1>
+  <p>#{outcome}</p>
+  <a href='/'> <p> Back to Home </p> </a>"
+end
+
+get("/dice/1/1000000") do
+  first_die = rand(1..1000000)
+  outcome = "You rolled a #{first_die}."
+  
+  "<h1>1d1000000</h1>
+  <p>#{outcome}</p>
+  <a href='/'> <p> Back to Home </p> </a>"
+end
